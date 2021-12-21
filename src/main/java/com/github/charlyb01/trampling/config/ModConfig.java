1f9bf5dfc2d2d720db38313e541435f3f24dfbed
Charly B <charly5bollinger@gmail.com>
2021-12-21 17:50:20 +0100

    Don't turn farmland into dirt
    If you wear leather boots (as with powder snow) or if your boots have feather falling enchantment (any level)
    Configurable in options

--- src/main/java/com/github/charlyb01/trampling/config/ModConfig.java
+++ src/main/java/com/github/charlyb01/trampling/config/ModConfig.java
@@ -6,6 +6,8 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = "trampling")
 public class ModConfig implements ConfigData {
+    public boolean leatherBoots = true;
+    public boolean featherFalling = true;
 
     public static ModConfig get() {
         return AutoConfig.getConfigHolder(ModConfig.class).getConfig();
---
+++ src/main/java/com/github/charlyb01/trampling/mixin/FarmlandBlockMixin.java
@@ -0,0 +1,39 @@
+package com.github.charlyb01.trampling.mixin;
+
+import com.github.charlyb01.trampling.config.ModConfig;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FarmlandBlock.class)
+public class FarmlandBlockMixin extends Block {
+    public FarmlandBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(method = "onLandedUpon", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"),
+            cancellable = true)
+    private void cancelTrampling(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance,
+                                 CallbackInfo ci) {
+        if ((ModConfig.get().leatherBoots
+                && ((LivingEntity) entity).getEquippedStack(EquipmentSlot.FEET).isOf(Items.LEATHER_BOOTS))
+                || (ModConfig.get().featherFalling
+                && EnchantmentHelper.getEquipmentLevel(Enchantments.FEATHER_FALLING, (LivingEntity) entity) > 0)) {
+            System.out.println("yop");
+            super.onLandedUpon(world, state, pos, entity, fallDistance);
+            ci.cancel();
+        }
+    }
+}
